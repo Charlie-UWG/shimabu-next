a2955a2a6028253db00f289c7ee4ddc90d6cbf2f
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-07-22 19:11:14 +0900

    🚸 APIを叩く際のローディング状態、エラー状態、空状態を表示させる。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,8 @@
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
+import { Posts } from "src/components/Posts";
 
 const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  console.log(posts);
   return (
     <div className="min-h-full px-2 flex flex-col justify-center items-center">
       <Head>
@@ -24,13 +11,7 @@ const Home = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {posts.length > 0 ? (
-        <ol className="list-decimal">
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
