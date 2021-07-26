2632b03308e0ea9e0f71c8dd1a1395e7cf25810e
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-07-26 23:33:57 +0900

    ⚡️ useReducerを使いました。

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,14 +1,35 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
+
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end": {
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    }
+    case "error": {
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+    default: {
+      throw new Error("no such as action type");
+    }
+  }
+};
 
 export const Posts = () => {
-  // const [posts, setPosts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
-  const [state, setState] = useState({
-    data: [],
-    loading: true,
-    error: null,
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getPosts = useCallback(async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -16,21 +37,10 @@ export const Posts = () => {
         throw new Error("データの取得に失敗しました");
       }
       const json = await res.json();
-      setState({
-        ...state,
-        data: json,
-        loading: false,
-      });
-      // setPosts(json);
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setState({
-        ...state,
-        loading: false,
-        error,
-      });
-      // setError(error);
+      dispatch({ type: "error", error });
     }
-    // setLoading(false);
   }, []);
 
   useEffect(() => {
