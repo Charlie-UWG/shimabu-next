88893e2dca956e98ea0903df80edf93e9192e501
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-07-31 14:58:50 +0900

    🚚 投稿系はpostsに集約、移動。

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <div className="flex justify-center w-full gap-4 m-5 p-6 border-b border-white">
-      <Link href="/">
+      <Link href="/posts">
         <a className="text-xl bg-gray-400 px-4 py-3 rounded-lg hover:text-blue-300 hover:bg-gray-300 transition-colors duration-300">
-          Index
+          Posts
         </a>
       </Link>
       <Link href="/about">
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Header } from "src/components/Header";
 import { Posts } from "src/components/Posts";
 
-const Home = (props) => {
+const Index = (props) => {
   return (
     <div className="min-h-full px-2 flex flex-col justify-center items-center">
       <Head>
@@ -16,4 +16,4 @@ const Home = (props) => {
   );
 };
 
-export default Home;
+export default Index;
