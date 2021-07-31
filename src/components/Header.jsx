236ade75a80dedd13adc120d76df5c862463a805
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-07-31 16:27:45 +0900

    🔥 不要なコードとファイルを削除

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <div className="flex justify-center w-full gap-4 m-5 p-6 border-b border-white">
+      <Link href="/">
+        <a className="text-xl bg-gray-400 px-4 py-3 rounded-lg hover:text-blue-300 hover:bg-gray-300 transition-colors duration-300">
+          Home
+        </a>
+      </Link>
       <Link href="/posts">
         <a className="text-xl bg-gray-400 px-4 py-3 rounded-lg hover:text-blue-300 hover:bg-gray-300 transition-colors duration-300">
           Posts
-        </a>
-      </Link>
-      <Link href="/about">
-        <a className="text-xl bg-gray-400 px-4 py-3 rounded-lg hover:text-blue-300 hover:bg-gray-300 transition-colors duration-300">
-          About
         </a>
       </Link>
     </div>
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,8 @@
-import { useBgColor } from "src/hooks/useBgColor";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
 import "src/styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
-  return <Component {...pageProps} {...counter} {...inputArray} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
